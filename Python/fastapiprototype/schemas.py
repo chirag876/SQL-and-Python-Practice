4d2fca78640e073f9c1586adad65fd3b50bab902
Chirag876 <chirag1706gupta@gmail.com>
2025-08-11 16:50:12 +0530

    removed unused code from schemas.py

--- Python/fastapiprototype/schemas.py
+++ Python/fastapiprototype/schemas.py
@@ -10,11 +10,6 @@ class UserCreate(UserBase):
     pass
 
 
-class UserUpdate(BaseModel):
-    name: str | None = None
-    email: EmailStr | None = None
-
-
 class UserRead(UserBase):
     id: int
 
